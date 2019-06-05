15ba1e9db835a90a8cf65deac4f5f44eca695a2c
laurab14 <laurabidois14@gmail.com>
2019-06-05 14:41:12 +0200

    base propre

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -16,7 +15,7 @@
 
     <body>
 
-=======
+
 <?php
 
  ?>
@@ -35,7 +34,6 @@
   </head>
 
   <body>
->>>>>>> f4c37235afb43890bf7a66d3f5aa4a5b5fd72bcd
 
 
 
@@ -44,7 +42,8 @@
 
 
 
-<<<<<<< HEAD
+
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -55,7 +54,6 @@
 
 
 </html>
-=======
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -67,4 +65,4 @@
 
 
   </html>
->>>>>>> f4c37235afb43890bf7a66d3f5aa4a5b5fd72bcd
+
