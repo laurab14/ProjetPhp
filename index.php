42cad5155ccd7254e6fab451497c898537ffa9a0
laurab14 <laurabidois14@gmail.com>
2019-06-05 23:08:07 +0200

    ça fonctionne pas mal

--- index.php
+++ index.php
@@ -12,13 +12,56 @@
   </head>
 
   <body>
- 
       <?php
       $file='source.xml';
       $xml = simplexml_load_file($file);
-      echo $xml->page[0]->content;
-?> 
+       
+      ?>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                  <?php
+                      
+                  for ($i = 0; $i <= 3; $i++) {
+                      $nmb = $i;
+                      ?>
+                  <li class="nav-item active">   
+                      <a class="nav-link" href="?/<?=$nmb?>"><?php  echo $xml->page[$i]->menu; ?></a>
+                      </li>
+                  <?php
+                  }
+                  ?>
+              </ul>
+          </div>
+      </nav>
 
+      <?php
+        if (isset($_GET['/0'])) {
+          echo $xml->page[0]->content;
+      } else {
+      
+      }
+      if (isset($_GET['/1'])){
+         echo $xml->page[1]->content;
+        } else {
+           
+      }
+        if (isset($_GET['/2'])){
+          echo $xml->page[2]->content;
+      } else {
+
+      }
+      
+        if (isset($_GET['/3'])){
+          echo $xml->page[3]->content;
+      } else {
+ 
+      }
+      ?>
+     
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -26,7 +69,4 @@
         <script src="https://code.angularjs.org/1.7.8/i18n/angular-locale_fr-fr.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     </body>
-
-
-
 </html>
